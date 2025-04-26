9bf1e2edafef625ddc5ad94cb89f4489a21210cf
Sarvarbek <ismatovsarvar76@gmail.com>
2025-04-26 06:47:24 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Services from "./components/services/Services";
-import Covid from "./components/covid/covid";
+import Covid from "./components/covid/Covid";
 import Resourse from "./components/resourse/Resourse";
 import Contact from "./components/contact/Contact";
 function App() {
